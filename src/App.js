dfd81839ca7cddd81959a3b8398ee458ca166310
Victor Dias <lzero1541@gmail.com>
2023-08-27 18:58:09 -0300

    using useState for create the component tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 const App = () => {
-  return <div className="container"></div>;
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "Estudar Programação",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Ler Documentos",
+      completed: true,
+    },
+  ]);
+
+  return (
+    <>
+      <div className="container"></div>
+    </>
+  );
 };
 
 export default App;
