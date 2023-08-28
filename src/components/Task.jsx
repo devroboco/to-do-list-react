ef3d97f5fc9afe60b5c1989ba080bb15e15f1e1b
Victor Dias <lzero1541@gmail.com>
2023-08-28 01:46:55 -0300

    creating the deletion tasks component

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { CgClose } from "react-icons/cg";
 
 import "./Task.css";
 
-const Task = ({ task, handleTaskClick }) => {
+const Task = ({ task, handleTaskClick, handleTaskDeletion }) => {
   return (
     <div
       className="task-container"
@@ -10,6 +11,15 @@ const Task = ({ task, handleTaskClick }) => {
     >
       <div className="task-title" onClick={() => handleTaskClick(task.id)}>
         {task.title}
+      </div>
+
+      <div className="buttons-container">
+        <button
+          className="remove-task-button"
+          onClick={() => handleTaskDeletion(task.id)}
+        >
+          <CgClose />
+        </button>
       </div>
     </div>
   );
